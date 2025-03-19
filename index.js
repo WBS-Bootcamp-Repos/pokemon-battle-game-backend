1417aaf1fe926b5ec3dc52ec572fcf3b0a34c961
Johanna <johanna.k.1996@googlemail.com>
2025-03-19 16:08:11 +0100

    added cors bc of react

--- index.js
+++ index.js
@@ -2,28 +2,36 @@ import express from "express";
 import "./db/db.js";
 import logger from "./utils/logger.js";
 import morgan from "morgan";
-import leaderboardRouter from "./routes/leaderboardRoutes.js"
+import leaderboardRouter from "./routes/leaderboardRoutes.js";
 import errorHandler from "./middleware/errorHandler.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT ?? 3003;
 
 const morganFormat = ":method :url :status :response-time ms";
 
+app.use(cors());
 app.use(
-    morgan(morganFormat, {
-        stream: {
-            write: (message) => {
-                const logObject = { method: message.split(" ")[0], url: message.split(" ")[1], status: message.split(" ")[2], responseTime: message.split(" ")[3], };
-                logger.info(JSON.stringify(logObject));
-            },
-        },
-    }));
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 app.use(express.json());
 app.use("/leaderboards", leaderboardRouter);
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
